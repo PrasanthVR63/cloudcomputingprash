786423c9ca8cf36c276c343c7b8725afc17f64ec
PrasanthVR63 <130088017+PrasanthVR63@users.noreply.github.com>
2023-05-21 13:16:24 +0100

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -4,4 +4,4 @@ apt install python3 apache2 -y
 wget https://raw.githubusercontent.com/PrasanthVR63/cloudcomputingprash/0878a890fc8c3c239c6216889328950611b3d4a2/apache2.conf -O /etc/apache2/apache2.conf
 apache2ctl restart
 wget https://raw.githubusercontent.com/PrasanthVR63/cloudcomputingprash/main/riskcalculator.py -P /var/www/html/
-chmod 755 /var/www/html/riskcalculator.py
+chmod 755 /var/www/html/simpleprint.py
